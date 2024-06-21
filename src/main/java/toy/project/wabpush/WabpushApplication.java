054ffb580636e9207446fae1fbceaadbf2c00c38
JaeYoung <jyoung_9154@naver.com>
2024-06-21 15:54:43 +0900

    [feat] Webpush_BackEnd 프로젝트 추가

---
+++ src/main/java/toy/project/wabpush/WabpushApplication.java
@@ -0,0 +1,13 @@
+package toy.project.wabpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WabpushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WabpushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/project/wabpush/WabpushApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.project.wabpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WabpushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
